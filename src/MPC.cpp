84a3ba563749196cb2b4e3f9daaad350e30dfab2
Amilendra Kodithuwakku <amilendra.kodithuwakku@gmail.com>
2018-09-16 16:06:31 +0100

    Initial commit with Google style formatting

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -30,9 +30,9 @@ class FG_eval {
   typedef CPPAD_TESTVECTOR(AD<double>) ADvector;
   void operator()(ADvector& fg, const ADvector& vars) {
     // TODO: implement MPC
-    // `fg` a vector of the cost constraints, `vars` is a vector of variable values (state & actuators)
-    // NOTE: You'll probably go back and forth between this function and
-    // the Solver function below.
+    // `fg` a vector of the cost constraints, `vars` is a vector of variable
+    // values (state & actuators) NOTE: You'll probably go back and forth
+    // between this function and the Solver function below.
   }
 };
 
